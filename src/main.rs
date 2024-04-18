139bf9938fc60d39ae26def95a14aca13cfd3c42
Francisco Neto <8297778+franmagneto@users.noreply.github.com>
2024-04-18 18:52:45 -0300

    feat: use a proper input handler

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,12 @@ use graphics::{canvas::Canvas, color::Color};
 use std::{num::NonZeroU32, rc::Rc, time::Duration};
 use winit::{
     dpi::LogicalSize,
-    event::{Event, KeyEvent, WindowEvent},
+    event::Event,
     event_loop::{ControlFlow, EventLoop},
     keyboard::{Key, NamedKey},
     window::WindowBuilder,
 };
+use winit_input_helper::WinitInputHelper;
 
 const WIDTH: u32 = 1024;
 const HEIGHT: u32 = 768;
@@ -22,6 +23,7 @@ const NS_PER_FRAME: u64 = 1_001_000_000 / 60;
 
 fn main() {
     let event_loop = EventLoop::new().unwrap();
+    let mut input = WinitInputHelper::new();
     let window = {
         let size = LogicalSize::new(WIDTH, HEIGHT);
         Rc::new(
@@ -48,50 +50,32 @@ fn main() {
 
     event_loop
         .run(move |event, elwt| {
-            match event {
-                Event::WindowEvent {
-                    window_id,
-                    event:
-                        WindowEvent::KeyboardInput {
-                            event: KeyEvent { logical_key, .. },
-                            ..
-                        },
-                } if window_id == window.id() => match logical_key {
-                    Key::Named(NamedKey::ArrowUp) => {
-                        ship.go_up();
-                    }
-                    Key::Named(NamedKey::ArrowDown) => {
-                        ship.go_down();
-                    }
-                    _ => {}
-                },
-                Event::AboutToWait => {
-                    canvas.set_color(Color::from_rgba(10, 15, 30, 0xff));
-                    canvas.clear();
-                    ship.render(&mut canvas);
-                    asteroid.render(&mut canvas);
-                    canvas.present();
-                }
-                Event::WindowEvent {
-                    window_id,
-                    event:
-                        WindowEvent::CloseRequested
-                        | WindowEvent::KeyboardInput {
-                            event:
-                                KeyEvent {
-                                    logical_key: Key::Named(NamedKey::Escape),
-                                    ..
-                                },
-                            ..
-                        },
-                } if window_id == window.id() => {
-                    elwt.exit();
-                }
-                _ => {}
+            if let Event::AboutToWait = event {
+                canvas.set_color(Color::from_rgba(10, 15, 30, 0xff));
+                canvas.clear();
+                ship.render(&mut canvas);
+                asteroid.render(&mut canvas);
+                canvas.present();
             }
 
-            ship.update();
-            asteroid.update();
+            if input.update(&event) {
+                if input.key_pressed_logical(Key::Named(NamedKey::Escape))
+                    || input.close_requested()
+                {
+                    elwt.exit();
+                }
+
+                if input.key_held_logical(Key::Named(NamedKey::ArrowUp)) {
+                    ship.go_up();
+                }
+
+                if input.key_held_logical(Key::Named(NamedKey::ArrowDown)) {
+                    ship.go_down();
+                }
+
+                ship.update();
+                asteroid.update();
+            }
         })
         .unwrap();
 }
